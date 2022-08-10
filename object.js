e2dda2b29a86479b978f4e9b97147360d37a8852
Rocko <codingwithrock@gmail.com>
2022-08-10 19:10:07 +0600

    object single value has changed

--- object.js
+++ object.js
@@ -1,12 +1,24 @@
-let objectBook =
+const objectBook =
 {
-    price: 100+"tk",
+    price: 100 + "tk",
     author: "by me",
     bookName: "Travel the World",
     category: "Adventure"
 }
 
-
-
-
 console.log(objectBook.bookName, objectBook.category, objectBook.price, objectBook.author);
+
+
+
+
+
+// * Object keys
+console.log(Object.keys(objectBook))
+
+
+
+// * Object Single value changing.
+
+objectBook.price = 200;
+console.log(objectBook.price);
+console.log(objectBook);
